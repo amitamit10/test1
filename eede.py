271b9c8101ef6e09c2a756025d29fcc600a17f2e
amit <amit.elgabsy@gmail.com>
2025-08-06 11:53:08 +0300

    added print

--- eede.py
+++ eede.py
@@ -1,4 +1,14 @@
-print("ffff")
-print("grwef")
-print(jfh)
-kss
+import pyautogui
+
+# הזזת העכבר למיקום
+pyautogui.moveTo(100, 100)
+
+# קליק שמאלי
+pyautogui.click()
+
+# גרירה
+pyautogui.dragTo(500, 500, duration=1)
+
+# קבלת מיקום העכבר
+x, y = pyautogui.position()
+print(f"Mouse at: {x}, {y}")
